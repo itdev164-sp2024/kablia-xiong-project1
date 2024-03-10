509244e06cac569cc53795b48763e670d5dae545
kabliax <103865881+kabliax@users.noreply.github.com>
2024-03-10 18:56:25 -0500

    Completed Activity 3

--- blog/src/pages/index.js
+++ blog/src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react"
 import { graphql, Link } from "gatsby"
+import { GatsbyImage } from "gatsby-plugin-image"
 
 import Layout from "../components/layout"
 import Seo from "../components/seo"
@@ -13,6 +14,14 @@ const IndexPage = ({ data }) => (
           data.allContentfulBlogPost.edges.map(edge => (
             <li key={edge.node.id}>
               <Link to={edge.node.slug}>{edge.node.title}</Link>
+              <div>
+              <GatsbyImage
+                image={edge.node.heroImage.gatsbyImageData}
+                />
+              </div>
+              <div>
+                {edge.node.body.childMarkdownRemark.excerpt}
+              </div>
             </li>
           ))
 
@@ -27,19 +36,29 @@ const IndexPage = ({ data }) => (
  * See: https://www.gatsbyjs.com/docs/reference/built-in-components/gatsby-head/
  */
 export const Head = () => <Seo title="Home" />
-
-export default IndexPage
-
 export const query = graphql`
   {
     allContentfulBlogPost {
       edges {
         node {
-          id
+          id 
           title
           slug
+          body {
+            childMarkdownRemark {
+              excerpt
+            }
+          }
+          heroImage {
+            gatsbyImageData(
+              layout: CONSTRAINED
+              placeholder: BLURRED
+              width: 300
+            )
+          }
         }
       }
     }
   }
 `
+export default IndexPage
